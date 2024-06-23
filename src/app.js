9356ae577aacc5e8823d7eef22990da1e630ecb5
erikathea <ethajes@gmail.com>
2024-06-23 19:18:51 -0300

    arrayBufferToBase64 test

--- src/app.js
+++ src/app.js
@@ -48,6 +48,16 @@ function sendSerializedData(serializedData) {
     });
 }
 
+function arrayBufferToBase64(buffer) {
+    let binary = '';
+    let bytes = new Uint8Array(buffer);
+    let len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+}
+
 function testSerializeFunction(serializedData) {
     // Verify the result by inspecting the buffer contents
     const view = new DataView(serializedData.buffer);
